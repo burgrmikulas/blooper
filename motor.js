c936471376a35f0f1739d3c13f6fa6fcf1de0034
Ben Ellentuck <ellentuckben@gmail.com>
2018-08-22 16:50:45 -0700

    making directions more intuitive - pushing up on controller moves the thing forward instead of back now

--- motor.js
+++ motor.js
@@ -14,20 +14,17 @@ const initMotor = (pin = 11, neutral = 50) => {
   }
 }
 
-const forward = (motor, speed, increment = 4) => {
-  console.log('power boost speed', increment)
-  speed += increment;
-  //speed = five.Fn.constrain(speed + increment, MIN_SPEED, MAX_SPEED);
-  console.log('max speed is', 100)
+const reverse = (motor, speed, decrement = 4) => {
+  speed += decrement;
+  //speed = five.Fn.constrain(speed + decrement, MIN_SPEED, MAX_SPEED);
   speed = Math.min(speed, 100);
-  console.log('speed after mathmin', speed)
   motor.speed(speed);
   return speed;
 }
 
-const reverse = (motor, speed, decrement = 4) => {
-  //speed = five.Fn.constrain(speed - decrement, MIN_SPEED, MAX_SPEED);
-  speed -= decrement;
+const forward = (motor, speed, increment = 4) => {
+  //speed = five.Fn.constrain(speed - increment, MIN_SPEED, MAX_SPEED);
+  speed -= increment;
   speed = Math.max(speed, 0);
   motor.speed(speed);
   return speed;
